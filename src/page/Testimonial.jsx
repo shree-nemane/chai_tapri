e097425d450721d69389cc049f75505cac8defbc
shree-nemane <shreenemane06@outlook.com>
2025-10-24 21:45:57 +0530

    modified:   src/components/home/Header.jsx 	modified:   src/page/Testimonial.jsx

--- src/page/Testimonial.jsx
+++ src/page/Testimonial.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Slider from 'react-slick';
-import truckImg from '../assets/testimonial/truck.png';
+import truckImg from '../assets/testimonial/Truck.png';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
